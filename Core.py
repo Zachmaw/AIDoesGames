84edbec73685de168b7e13aab8abcf3ffa422298
Zachmaw <zachmaw28@gmail.com>
2023-06-21 03:15:05 -0700

    Update Core.py

---
+++ Core.py
@@ -0,0 +1,8 @@
+# Obtain settings regarding the size/position of the game window.
+# load a network from text file or pickle or something
+#
+
+
+class MyModel():
+    def __init__(self) -> None:
+        pass
